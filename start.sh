078612124eb5bd2a94fca07d291c19dbf96a426b
randomprogramming <dgoluban38@gmail.com>
2025-02-24 22:53:04 +0100

    disabled deploy migrate for now

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e 
 
 echo "Running Prisma Migrations..."
-bun prisma:migrate:deploy
+# bun prisma:migrate:deploy
 # TODO: For now do migrate dev, because i don't include migrations in git history this is a bit fucky, later you can remove this
 # once we start tracking migation files
 bun prisma:migrate:dev -- --skip-generate
